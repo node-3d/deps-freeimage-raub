5fd8742adda1cc21eab3e652c16c0c4066ac605f
Luis Blanco <luisblanco1337@gmail.com>
2023-01-11 21:01:49 +0400

    Try 2022 toolset

--- src/windows.cmd
+++ src/windows.cmd
@@ -1,4 +1,4 @@
-msbuild /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v141 src\FreeImage\FreeImage.2013.vcxproj
+msbuild /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v143 src\FreeImage\FreeImage.2013.vcxproj
 
 copy /y src\FreeImage\Dist\x64\FreeImage.dll src\build\FreeImage.dll
 copy /y src\FreeImage\Dist\x64\FreeImage.lib src\build\FreeImage.lib
