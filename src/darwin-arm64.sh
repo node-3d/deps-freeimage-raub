fbd74f6f070ddcc404477b43c064428dbe6e2362
Luis Blanco <luisblanco1337@gmail.com>
2024-06-01 19:32:54 +0400

    Update osx build 7

--- src/darwin-arm64.sh
+++ src/darwin-arm64.sh
@@ -8,10 +8,10 @@
 			CPP_X86_64='g++ -w' \
 			CC_X86_64='gcc -w' \
 			COMPILERFLAGS_X86_64='-arch arm64 -D__ANSI__ -DDISABLE_PERF_MEASUREMENT' \
-			LIBRARIES_X86_64='-flat_namespace -install_name "@rpath/freeimage.dylib" -Wl,-syslibroot /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.1.sdk' \
-			INCLUDE_X86_64='-isysroot /Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.1.sdk' \
-			libfreeimage-3.18.0.dylib-x86_64 \
-			>/dev/null
+			LIBRARIES_X86_64='-flat_namespace -install_name "@rpath/freeimage.dylib" -Wl,-syslibroot /Applications/Xcode_12.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
+			INCLUDE_X86_64='-isysroot /Applications/Xcode_12.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
+			libfreeimage-3.18.0.dylib-x86_64
+			# >/dev/null
 	)
 
 	mv FreeImage/libfreeimage-3.18.0.dylib-x86_64 build/freeimage.dylib
