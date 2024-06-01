7bbe98c62c31e96bf9a4c1b90b0b878cfc5f38c9
Luis Blanco <luisblanco1337@gmail.com>
2024-06-01 17:47:39 +0400

    Update osx build 4

--- src/linux.sh
+++ src/linux.sh
@@ -3,8 +3,8 @@
 	(
 		cd FreeImage
 		make \
-			CPP_X86_64='g++ -w -std=c++11' \
-			CC_X86_64='gcc -w'
+			CXX='g++ -std=c++11 -w' \
+			CC='gcc -w'
 	)
 
 	mv FreeImage/Dist/libfreeimage-3.18.0.so build/libfreeimage.so.3
