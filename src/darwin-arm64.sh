a31ed65bb0153351bc9863e8cfb128bd2398390f
Luis Blanco <luisblanco1337@gmail.com>
2024-06-01 20:01:13 +0400

    Update osx build 8

--- src/darwin-arm64.sh
+++ src/darwin-arm64.sh
@@ -1,18 +1,15 @@
 (
 	cd src
 	(
+		rm FreeImage/Makefile.osx
+		mv Makefile.osx-arm FreeImage/Makefile.osx
+		
 		cd FreeImage
 		
 		export DYLIB_INSTALL_NAME_BASE=@rpath
-		make -f Makefile.osx \
-			CPP_X86_64='g++ -w' \
-			CC_X86_64='gcc -w' \
-			COMPILERFLAGS_X86_64='-arch arm64 -D__ANSI__ -DDISABLE_PERF_MEASUREMENT' \
-			LIBRARIES_X86_64='-flat_namespace -install_name "@rpath/freeimage.dylib" -Wl,-syslibroot /Applications/Xcode_12.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
-			INCLUDE_X86_64='-isysroot /Applications/Xcode_12.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
-			libfreeimage-3.18.0.dylib-x86_64
+		make -f Makefile.osx
 			# >/dev/null
 	)
 
-	mv FreeImage/libfreeimage-3.18.0.dylib-x86_64 build/freeimage.dylib
+	mv FreeImage/libfreeimage-3.18.0.dylib-arm64 build/freeimage.dylib
 )
