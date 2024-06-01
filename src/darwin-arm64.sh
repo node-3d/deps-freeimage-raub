2ff1495fb0193b7e0e4a11ac038585120e6aef7e
Luis Blanco <luisblanco1337@gmail.com>
2024-06-01 21:50:11 +0400

    Update osx build 17

--- src/darwin-arm64.sh
+++ src/darwin-arm64.sh
@@ -8,9 +8,10 @@
 		
 		export DYLIB_INSTALL_NAME_BASE=@rpath
 		make -f Makefile.osx \
+			libfreeimage-3.18.0.dylib-arm64
 			>/dev/null
 	)
 	
 	>&2 ls FreeImage
-	mv FreeImage/libfreeimage-3.18.0.dylib build/freeimage.dylib
+	mv FreeImage/libfreeimage-3.18.0.dylib-arm64 build/freeimage.dylib
 )
