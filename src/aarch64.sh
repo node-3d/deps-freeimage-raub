1d9df58080d407ef69e2a0bb0a97c9127de0cee0
Luis Blanco <luisblanco1337@gmail.com>
2025-02-28 10:14:56 +0400

    Try custom aarch64 build

--- src/aarch64.sh
+++ src/aarch64.sh
@@ -1,1 +1,10 @@
-chmod +x src/linux.sh && src/linux.sh
+(
+	cd src
+	(
+		cd FreeImage
+		make -f Makefile.gnu \
+			CXXFLAGS='-O3 -fPIC -fexceptions -fvisibility=hidden -Wno-ctor-dtor-privacy -DPNG_ARM_NEON_OPT=0'
+	)
+
+	mv FreeImage/Dist/libfreeimage-3.18.0.so build/libfreeimage.so.3
+)
